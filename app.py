852e3f993115758cbddd84a65ec7fe664256a8b4
GaryKim <moduedu@gmail.com>
2023-02-08 16:23:39 +0900

    web url path change

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """
+    <h1>Gary Kim</h1>
+    <p>Hello, World!</p>"""
 
 
 app.run(host="localhost",port=5001)
